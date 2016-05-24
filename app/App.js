a999a6d15fbeac34d45db4881d2ccb5aa64da3a0
Jhon Majoor <jhon@blancframe.com>
2016-05-24 23:17:19 +0200

    Add navigation

--- app/App.js
+++ app/App.js
@@ -50,6 +50,38 @@ export default class darkchat extends Component {
         });
     }
 
+    renderScene(route, navigator) {
+        return <route.component navigator={navigator} {...route.passProps} />
+    }
+
+    configureScene(route, routeStack) {
+        return Navigator.SceneConfigs.HorizontalSwipeJump
+    }
+
+    render() {
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+        return (
+            <Navigator
+                configureScene={this.configureScene}
+                initialRoute={{component: ChatList}}
+                renderScene={this.renderScene}
+              />
+        );
+    }
+
+    renderLoadingView() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading...
+                </Text>
+            </View>
+        )
+    }
+
+    /*
     render() {
         if (!this.state.loaded) {
             return this.renderLoadingView();
@@ -67,36 +99,9 @@ export default class darkchat extends Component {
               />
         );
     }
+    */
 
-    renderBlabla() {
-        return (
-            <View style={styles.addChat}>
-                <Image source={require('./images/add.png')} style={styles.add}>
-                </Image>
-            </View>
-        )
-    }
-
-    renderLoadingView() {
-        return (
-            <View style={styles.container}>
-                <Text>
-                    Loading...
-                </Text>
-            </View>
-        )
-    }
-
-    _onPressCell() {
-        this.refs.nav.push(
-            <View>
-                <Text>
-                    Testt
-                </Text>
-            </View>
-        )
-    }
-
+    /*
     renderChat(chat) {
         return (
             <TouchableHighlight onPress={this._onPressCell} underlayColor='pink'>
@@ -113,7 +118,51 @@ export default class darkchat extends Component {
             </TouchableHighlight>
         );
     }
+    */
 }
+
+const ChatList = React.createClass({
+    _navigate(name, type='Normal') {
+        this.props.navigator.push({
+            component: ChatRoom,
+            passProps: {
+                name: name
+            },
+            type: type
+        })
+    },
+    render() {
+        return (
+            <TouchableHighlight onPress={()=> this._navigate('Testtt')}>
+                <View  style={styles.container}>
+                    <View style={styles.leftContainer}>
+                        <Text style={styles.roomName}>Office Talk</Text>
+                        <Text style={styles.status}>active</Text>
+                    </View>
+                    <View style={styles.righContainer}>
+                        <Image source={require('./images/arrow.png')} style={styles.arrow}>
+                        </Image>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+});
+
+const ChatRoom = React.createClass({
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.roomName}>
+                    Hello {this.props.name}
+                </Text>
+                <TouchableHighlight onPress={()=> this.props.navigator.pop()}>
+                    <Text style={styles.roomName}> Go back </Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
+})
 
 const styles = StyleSheet.create({
   container: {
