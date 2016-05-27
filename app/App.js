fea3122bf26a67ae929a1dcf605f9184d337c942
Jhon Majoor <jhon@blancframe.com>
2016-05-27 21:53:16 +0200

    Move components to own dir/file

--- app/App.js
+++ app/App.js
@@ -1,9 +1,3 @@
-
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 import React, {
   Component,
   Image,
@@ -14,44 +8,16 @@ import React, {
   Navigator,
   TouchableHighlight
 } from 'react-native';
+import Channels from './components/Channels';
 
-let REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
-
-let MOCKED_CHAT_DATA = [
-    {roomName: 'Office Talk', status: 'activity', id:1},
-    {roomName: 'Gamers for live', status: 'no activity', id:2},
-    {roomName: '(╯°□°）╯︵ ┻━┻', status: 'activity', id:7},
-    {roomName: 'The next office manager', status: 'no activity', id:4},
-    {roomName: 'Best kept secrets', status: 'activity', id:3},
-    {roomName: 'All about coffee', status: 'no activity', id:5},
-    {roomName: 'Just rage!!', status: 'activity', id:6},
-    {roomName: '┬──┬ ノ( ゜-゜ノ)', status: 'no activity', id:8},
-];
+const ROUTES = {
+    channels: Channels
+}
 
 export default class darkchat extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2) => row1 !== row2,
-            }),
-            loaded: false,
-        };
-    }
-
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData() {
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(MOCKED_CHAT_DATA),
-            loaded: true,
-        });
-    }
-
     renderScene(route, navigator) {
-        return <route.component navigator={navigator} {...route.passProps} />
+        let Component = ROUTES[route.name];
+        return <Component navigator={navigator} {...route.passProps} />
     }
 
     configureScene(route, routeStack) {
@@ -59,29 +25,26 @@ export default class darkchat extends Component {
     }
 
     render() {
-        if (!this.state.loaded) {
-            return this.renderLoadingView();
-        }
         return (
             <Navigator
                 configureScene={this.configureScene}
-                initialRoute={{component: ChatList}}
+                initialRoute={{name: 'channels'}}
                 renderScene={this.renderScene}
               />
         );
     }
 
+    /*
     renderLoadingView() {
         return (
             <View style={styles.container}>
-                <Text>
-                    Loading...
-                </Text>
+            <Text>
+            Loading...
+            </Text>
             </View>
         )
     }
 
-    /*
     render() {
         if (!this.state.loaded) {
             return this.renderLoadingView();
@@ -120,7 +83,7 @@ export default class darkchat extends Component {
     }
     */
 }
-
+/*
 const ChatList = React.createClass({
     _navigate(name, type='Normal') {
         this.props.navigator.push({
@@ -163,54 +126,18 @@ const ChatRoom = React.createClass({
         )
     }
 })
+*/
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#1E1E1E',
-    paddingTop: 10,
-    paddingRight: 20,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(255, 255, 255, 0.1)'
-  },
-  leftContainer: {
-      flex: 1,
-      alignItems: 'flex-start',
-  },
-  righContainer: {
-      flex: 1,
-      alignItems: 'flex-end',
-      paddingTop: 15,
-  },
-  thumbnail: {
-      width: 53,
-      height: 81
-  },
-  roomName: {
-      fontSize: 20,
-      marginBottom: 8,
-      textAlign: 'left',
-      color: '#ffffff'
-  },
-  status: {
-      textAlign: 'left',
-      color: '#ABABAB',
-      fontSize: 12
-  },
-  listView: {
-      paddingTop: 20,
-      backgroundColor: '#919191'
-  },
-  arrow: {
-      width: 11.12,
-      height: 19.41
-  },
-  addChat: {
+    container: {
       flex: 1,
       flexDirection: 'row',
-      padding: 30
-  }
+      backgroundColor: '#1E1E1E',
+      paddingTop: 10,
+      paddingRight: 20,
+      paddingBottom: 10,
+      paddingLeft: 20,
+      borderBottomWidth: 1,
+      borderBottomColor: 'rgba(255, 255, 255, 0.1)'
+    }
 });
